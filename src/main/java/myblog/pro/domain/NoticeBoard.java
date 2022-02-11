1e91025dd2d0f8a41fe752f653367ef1ec9f9a35
alisyabob <sjs939883@gmail.com>
2022-02-12 00:32:40 +0900

    [ADD] 게시글 엔티티 수정(NoBlank, protected, builder)

--- src/main/java/myblog/pro/domain/NoticeBoard.java
+++ src/main/java/myblog/pro/domain/NoticeBoard.java
@@ -1,25 +1,46 @@
 package myblog.pro.domain;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import myblog.pro.dto.NoticeBoardRequestDto;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NoticeBoard {
 
     @Id @GeneratedValue
     @Column(name = "board_id")
     private Long id;
 
+    @NotBlank(message = "제목은 공백일수가 없어^^")
     private String title;
 
+    @NotBlank(message = "저자는 공백일수가 없어^^")
     private String writer;
 
+    @NotBlank(message = "내용은 공백일수가 없어^^")
+    @Column(columnDefinition = "TEXT")
     private String content;
 
     private LocalDateTime boardDate;
 
     @OneToMany(mappedBy = "board", cascade = CascadeType.ALL,orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
+
+    @Builder
+    public NoticeBoard(String title, String writer, String content, LocalDateTime boardDate){
+        this.title = title;
+        this.writer = writer;
+        this.content = content;
+        this.boardDate = boardDate;
+    }
+
 }
