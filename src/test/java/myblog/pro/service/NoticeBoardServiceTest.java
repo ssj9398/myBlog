0fe64ad67ec3c78ef31d8268065912976ac0eee4
alisyabob <sjs939883@gmail.com>
2022-02-12 21:58:07 +0900

    [TEST] 게시글 단건 조회 테스트코드 완료

--- src/test/java/myblog/pro/service/NoticeBoardServiceTest.java
+++ src/test/java/myblog/pro/service/NoticeBoardServiceTest.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
-@Transactional
+@Transactional(readOnly = true)
 class NoticeBoardServiceTest {
 
     @Autowired
@@ -40,6 +40,7 @@ class NoticeBoardServiceTest {
 
     @Test
     void findPostAll() {
+        //given
         NoticeBoard noticeBoard = NoticeBoard.builder()
                 .writer("저자")
                 .content("내용")
@@ -47,7 +48,25 @@ class NoticeBoardServiceTest {
                 .boardDate(LocalDateTime.now())
                 .build();
         noticeBoardRepository.save(noticeBoard);
+        //when
         List<NoticeBoard> allByOrderByBoardDateDesc = noticeBoardRepository.findAllByOrderByBoardDateDesc();
+        //then
         assertThat(allByOrderByBoardDateDesc.size()).isEqualTo(5);
+    }
+
+    @Test
+    void findPostOne(){
+        //given
+        NoticeBoard noticeBoard = NoticeBoard.builder()
+                .writer("저자")
+                .content("내용")
+                .title("제목")
+                .boardDate(LocalDateTime.now())
+                .build();
+        NoticeBoard saveBoard = noticeBoardRepository.save(noticeBoard);
+        //when
+        Optional<NoticeBoard> findBoard = noticeBoardRepository.findById(saveBoard.getId());
+        //then
+        assertThat(findBoard.get().getId()).isSameAs(saveBoard.getId());
     }
 }
