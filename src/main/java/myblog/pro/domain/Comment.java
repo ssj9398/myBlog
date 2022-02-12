a1c7893336d59c67bc018c24696355e4c34dc71e
alisyabob <sjs939883@gmail.com>
2022-02-12 16:58:32 +0900

    [FIX] 댓글 엔티티 수정 (Getter, protected, builder)

--- src/main/java/myblog/pro/domain/Comment.java
+++ src/main/java/myblog/pro/domain/Comment.java
@@ -1,20 +1,40 @@
 package myblog.pro.domain;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
 
     @Id @GeneratedValue
     @Column(name = "comment_id")
     private Long id;
 
-    private Long contents;
-
-    private LocalDateTime commentDate;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id")
     private NoticeBoard board;
+
+    @NotBlank
+    @Column(columnDefinition = "TEXT")
+    private String contents;
+
+    private LocalDateTime commentDate;
+
+    @Builder
+    public Comment(String contents, LocalDateTime commentDate){
+        this.contents = contents;
+        this.commentDate = commentDate;
+    }
+
+    public void setPost(NoticeBoard noticeBoard){
+        this.board = noticeBoard;
+    }
 }
