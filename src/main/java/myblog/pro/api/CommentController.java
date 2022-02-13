e5928adcdf40f706ada20c6758f298fbad10cd9a
alisyabob <sjs939883@gmail.com>
2022-02-14 03:26:22 +0900

    [FIX] 패키지 변경

--- src/main/java/myblog/pro/api/CommentController.java
+++ src/main/java/myblog/pro/api/CommentController.java
@@ -1,9 +1,12 @@
-package myblog.pro.controller;
+package myblog.pro.api;
 
 import lombok.RequiredArgsConstructor;
 import myblog.pro.dto.CommentRequestDto;
 import myblog.pro.dto.CommentResponseDto;
+import myblog.pro.model.Success;
 import myblog.pro.service.CommentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.stream.Stream;
@@ -15,8 +18,9 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/comment/{post_id}")
-    public void commentAdd(@PathVariable Long post_id, @RequestBody CommentRequestDto commentRequestDto){
+    public ResponseEntity<Success> commentAdd(@PathVariable Long post_id, @RequestBody CommentRequestDto commentRequestDto){
         commentService.addComment(post_id, commentRequestDto);
+        return new ResponseEntity<>(new Success(true,"댓글 등록 완료!"), HttpStatus.OK);
     }
 
     @GetMapping("/comment/{post_id}")
@@ -27,12 +31,14 @@ public class CommentController {
     }
 
     @DeleteMapping("/comment/{comment_id}")
-    public void commentRemove(@PathVariable Long comment_id){
+    public ResponseEntity<Success> commentRemove(@PathVariable Long comment_id){
         commentService.removeComment(comment_id);
+        return new ResponseEntity<>(new Success(true,"댓글 삭제 완료!"), HttpStatus.OK);
     }
 
     @PatchMapping("/comment/{comment_id}")
-    public void commentModify(@PathVariable Long comment_id, @RequestBody CommentRequestDto commentRequestDto){
+    public ResponseEntity<Success> commentModify(@PathVariable Long comment_id, @RequestBody CommentRequestDto commentRequestDto){
         commentService.modifyComment(comment_id,commentRequestDto);
+        return new ResponseEntity<>(new Success(true,"댓글 수정 성공!"), HttpStatus.OK);
     }
 }
