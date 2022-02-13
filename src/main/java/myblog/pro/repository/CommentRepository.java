004b20793301486e193e24591f96fe3c3f18b48b
alisyabob <sjs939883@gmail.com>
2022-02-14 05:08:39 +0900

    [FIX] 댓글 시간 내림차순으로 쿼리 추가

--- src/main/java/myblog/pro/repository/CommentRepository.java
+++ src/main/java/myblog/pro/repository/CommentRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    @Query("select c from Comment c where c.board.id=:post_id")
+    @Query("select c from Comment c where c.board.id=:post_id order by c.commentDate desc ")
     List<Comment> findByComment(@Param("post_id") Long post_id);
 }
